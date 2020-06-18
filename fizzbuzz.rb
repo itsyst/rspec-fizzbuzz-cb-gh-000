51ed88e1eae4a9aa48796014ab14ad62e66b2fbb
Khaled El Hamzi <khaled.elhamzi.syne19lin@tucsweden.se>
2020-06-18 10:17:22 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,10 +3,14 @@
 def fizzbuzz(int)
   if int % 3 == 0
      "Fizz"
+   end
   elsif int % 5 == 0
      "Buzz"
+   end
   elsif int % 15 == 0
      "FizzBuzz"
+   end
   else
     return nil
+  end
 end
